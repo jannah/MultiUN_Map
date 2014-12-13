5f3c81da5710676f63c6670fe3d1cf667d898bb8
jannah <hassan.jannah@gmail.com>
2014-12-13 13:34:11 -0800

    added histogram support

--- app/static/_js/script.js
+++ app/static/_js/script.js
@@ -5,3 +5,85 @@
  */
 
 
+Histogram = function(target, data, width, height, title) {
+    console.log(data);
+//    console.log($(this).parent());
+
+//    $(this).parent().append("TESTING")
+    data.map(function(d, i, a) {
+        a[i]['label'] = d.values + ' ' + d.key;
+    });
+    console.log(data);
+    var w = width, titleH = 30, h = height - titleH;
+
+
+    var max = d3.max(data, function(d)
+    {
+        //            console.log(d);
+        return d.values;
+    });
+    var min = d3.min(data, function(d)
+    {
+        return d.values;
+    });
+    var yScale = d3.scale.ordinal().rangeBands([0, h], .1, 1)
+            .domain(data.map(function(d)
+            {
+                return d.label;
+            }));
+//    var yLabels = data.map(function(d) {
+////        console.log(d);
+//        return d.values + ' ' + d.key;
+//    })
+//    console.log(yLabels)
+    var xScale = d3.scale.linear().domain([0, max]).range([0, w]);
+    var yAxis = d3.svg.axis().scale(yScale).orient('left')
+//            .tickValues(yLabels);
+    var svg = d3.select(target).append('svg')
+            .attr({
+                height: height, width: width, class: 'histogram'}
+            );
+    var title = svg.append('g').append('text')
+            .attr({
+                x: w / 2, y: titleH
+            })
+            .style({'text-anchor': 'middle'})
+            .text(title);
+    var bars = svg.append('g')
+            .attr("transform", "translate(" + 0 + "," + titleH + ")")
+            .selectAll('rect')
+            .data(data).enter()
+            .append('rect')
+            .attr({
+                x: 0,
+                y: function(d) {
+                    return yScale(d.label);
+                },
+                height: yScale.rangeBand(),
+                width: function(d) {
+                    return xScale(d.values);
+                }
+            })
+            .on('click',function(d) {
+                alert(d.label);
+            })
+    var yAxisGroup = svg.append('g').attr(
+            {
+                class: "y axis",
+                width: w,
+                height: h
+            }).append("g")
+            .attr("transform", "translate(" + 0 + "," + titleH + ")")
+            .call(yAxis)
+            .selectAll("text")
+            .style("text-anchor", "start")
+            .attr({"dx":"2em", "dy": ".2em",'unselectable':'on'})
+            .classed('unselectable', true)
+               .on('click',function(d) {
+                alert(d);
+            })
+//            .attr("transform", function(d) {
+//                return "rotate(45)";
+//            });
+
+}
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from app import app
 from app.forms import SearchForm
 import json
 from flask import Flask, request, redirect,flash, url_for,render_template
-
+import nltk
 from modules import multi_un_module as mun
 print 'READY'
 @app.route('/', methods=['GET', 'POST'])
@@ -20,13 +20,23 @@ def home():
 def do_search(q):
     print ' IN SEARCH'
     documents = mun.get_documents(term=q)
-    print documents
-    print type(documents)
-    for doc in documents:
-        print type(doc)
+#    print documents
+#    print type(documents)
+#    for doc in documents:
+#        print doc
+#        
+#        print documents[doc]['scrape']['Subjects']
+##        break
+        
+    subjects_list = [documents[doc]['scrape']['Subjects']
+            for doc in documents 
+            if 'scrape' in documents[doc]
+            and 'Subjects' in documents[doc]['scrape']]
+    subjects = [ subject for subjects in subjects_list for subject in subjects]
+#    subjects_fd = [{'key':key, 'value':value} for (key,value) in nltk.FreqDist(subjects).items()]
         
     
-    return render_template('results.html', documents=documents, form = SearchForm())
+    return render_template('results.html', documents=documents, subjects=subjects, form = SearchForm())
     
     
 @app.route('/show')
@@ -35,6 +45,5 @@ def show():
     print doc_name
     
     document = mun.get_document(doc_name=doc_name, include_content = True)
-    
     return render_template('doc.html', doc_name=doc_name, doc=document.itervalues().next(), mun=mun)
-    return 'showing'
+    
